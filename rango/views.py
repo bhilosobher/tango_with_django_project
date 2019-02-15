e41aa7308f1872055f09b7de0e1ad18a25df8fea
bhilosobher <casian.danila@yahoo.com>
2019-02-15 16:31:08 +0000

    finished chapter 10

--- rango/views.py
+++ rango/views.py
@@ -14,10 +14,12 @@ def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories':category_list,'pages':page_list}
+  
+    #add cookie information to the respose
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
     #get response objects earlier so we can add cookie information
     response = render(request, 'rango/index.html', context_dict)
-    #add cookie information to the respose
-    visitor_cookie_handler(request,response)
     # return cookie-updating response
     return response    
   
@@ -150,17 +152,28 @@ def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
 
-def visitor_cookie_handler(request, response):
-    visits = int(request.COOKIES.get('visits','1'))
-    
-    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
-    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
-    
-    if (datetime.now() - last_visit_time).days > 0:
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()) )
+
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], "%Y-%m-%d %H:%M:%S")
+    #last_visit_time = datetime.now()
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).seconds > 0:
         visits = visits + 1
-        response.set_cookie('last_visit', str(datetime.now()))
+        #update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
     else:
-        response.set_cookie('last_visit', last_visit_cookie)
-        
-    response.set_cookie('visits', visits)
+        visits = 1
+        # set the last visit cookie 
+        request.session['last_visit'] = last_visit_cookie
+    # update/set the visits cookie
+    request.session['visits'] = visits
+    
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
         
