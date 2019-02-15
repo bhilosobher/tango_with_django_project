6efbcdf9bd78f8742ff49ad9038cc4cb52ee1945
bhilosobher <casian.danila@yahoo.com>
2019-02-15 16:23:05 +0000

    chapter 9 section 6

--- rango/views.py
+++ rango/views.py
@@ -7,17 +7,25 @@ from django.contrib.auth.decorators import login_required
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm
 from rango.forms import UserForm, UserProfileForm
+from datetime import datetime
 
 def index(request):
-    
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories':category_list,'pages':page_list}
-    # render and send back response
-    return render(request, 'rango/index.html', context_dict)
-    #return HttpResponse("Rango says hey there partner!<br/> <a href='/rango/about/'>About</a>")
+    #get response objects earlier so we can add cookie information
+    response = render(request, 'rango/index.html', context_dict)
+    #add cookie information to the respose
+    visitor_cookie_handler(request,response)
+    # return cookie-updating response
+    return response    
+  
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print('TEST COOKIE WORKED!')
+        request.session.delete_test_cookie()
     return render(request, 'rango/about.html', {})
     #return HttpResponse("Rango says here is the about page.<br/> <a href='/rango/'>Index</a>")
     
@@ -134,10 +142,25 @@ def user_login(request):
     
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html', {})
 
 @login_required
 
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+def visitor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits','1'))
+    
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+    
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        response.set_cookie('last_visit', last_visit_cookie)
+        
+    response.set_cookie('visits', visits)
+        
